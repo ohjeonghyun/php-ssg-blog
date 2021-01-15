9accdb4c6f8cf0be113e5983a9db009646732519
ohejonghyun <gotqlc_@naver.com>
2021-01-15 17:10:12 +0900

    PHP변경

---
+++ about.php
@@ -0,0 +1,25 @@
+          
+<?php
+require_once "head.php";
+?>       
+
+<!--메인 시작 -->
+
+  <main>
+    <section class="section-latest-articles con-min-width">
+      <dh1 class="con">
+          <span>About</span>
+          </h1>
+       
+    </section>
+  </main>
+
+
+
+    
+<?php
+require_once "foot.php";
+?>
+  
+    <!--메인 끝 -->
+        
---
+++ foot.php
@@ -0,0 +1,25 @@
+
+<?php
+require_once "head.php";
+?>
+<div class="bottom-bar con-min-width">
+          <div class="con flex flex-jc-c">
+           <a href="#" class="logo">
+              <span><i class="fab fa-envira"></i></span>
+              <span>Design Jasmin</span>
+            </a>
+          </div>
+          </div>
+        </div>
+          
+        
+        
+        </div>
+
+
+
+        
+        
+        
+ 
+        <!-- 버텀 바 끝 --<
