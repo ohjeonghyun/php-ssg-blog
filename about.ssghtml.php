17880f90460e01fa29fc852a16a5c3a36c682d19
ohjeonghyun <gotqlc_@naver.com>
2021-01-18 15:58:11 +0900

    Update about.ssghtml.php

--- about.ssghtml.php
+++ about.ssghtml.php
@@ -8,7 +8,7 @@ require_once "head.php";
 
   <main>
     <section class="section-latest-articles con-min-width">
-      <dh1 class="con">
+      <h1 class="con">
           <span>About</span>
           </h1>
        
