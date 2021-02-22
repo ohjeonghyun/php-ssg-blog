83493d3a890601b2ecb33b57f176e996e4f72532
ohjeonghyun <72539543+ohjeonghyun@users.noreply.github.com>
2021-02-22 16:38:55 +0900

    f

--- docs/pf/mobile/index.js
+++ docs/pf/mobile/index.js
@@ -1,125 +1,125 @@
 gsap.from('.section-2', {
-    y:-100,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-2',
-      markers:true,
-      start:'60% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  gsap.from('.section-4', {
-    x:-600,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-4',
-      markers:true,
-      start:'50% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  gsap.from('.section-5', {
-    x:400,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-5',
-      markers:true,
-      start:'50% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  
-  gsap.from('.section-6', {
-    x:-400,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-6',
-      markers:true,
-      start:'50% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  gsap.from('.section-7', {
-    x:-400,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-7',
-      markers:true,
-      start:'50% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  
-  
-  
-  
-  gsap.from('.section-9', {
-    x:-400,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.section-9',
-      markers:true,
-      start:'80% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  
-  gsap.from('.last-page', {
-    x:-300,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.last-page',
-      markers:true,
-      start:'80% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  
-  gsap.from('.category', {
-    x:-600,
-    opacity:0,
-    duration:1,
-    scrollTrigger:{
-      trigger:'.category',
-      markers:true,
-      start:'40% bottom',
-      toggleActions: 'restart none none none'
-    }
-  });
-  
-  
-  const Timeline__Section2__1 = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.section-8',
-      markers:true,
-      start:'top 70%',
-      end:'bottom top',
-      toggleActions: "restart none none none"
-    }
-  });
-  
-  Timeline__Section2__1.from('.section-2__txt-1', {
-    opacity:0,
-    y:-80,
-    duration:1
-  });
-  
-  Timeline__Section2__1.from('.section-2__txt-2', {
-    opacity:0,
-    y:80,
-    duration:1
-  }, "-=0.5");
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-2',
+    markers:true,
+    start:'40% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-4', {
+  x:-600,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-4',
+    markers:true,
+    start:'50% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-5', {
+  x:400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-5',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.section-6', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-6',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-7', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-7',
+    markers:true,
+    start:'30% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+
+
+
+gsap.from('.section-9', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-9',
+    markers:true,
+    start:'30% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.last-page', {
+  x:-300,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.last-page',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.category', {
+  x:-600,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.category',
+    markers:true,
+    start:'20% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+const Timeline__Section2__1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.section-8',
+    markers:true,
+    start:'top 70%',
+    end:'bottom top',
+    toggleActions: "restart none none none"
+  }
+});
+
+Timeline__Section2__1.from('.section-2__txt-1', {
+  opacity:0,
+  y:-80,
+  duration:1
+});
+
+Timeline__Section2__1.from('.section-2__txt-2', {
+  opacity:0,
+  y:80,
+  duration:1
+}, "-=0.5");
