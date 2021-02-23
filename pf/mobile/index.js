070a18542aa4e2888088a8073ec5643f8882ac3a
ohjeonghyun <72539543+ohjeonghyun@users.noreply.github.com>
2021-02-23 14:12:53 +0900

    수정

--- pf/mobile/index.js
+++ pf/mobile/index.js
@@ -1,261 +1,125 @@
-body,
-ul,
-li {
-  margin: 0;
-  padding: 0;
-}
-
-.con {
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.section {
-  position:relative;
-  background-repeat:no-repeat;
-  background-size:cover;
-  background-position:center;
-}
-
-.section > .img > img {
-  width:100%;
-  display:block;
-}
-
-.section-1 {
-  background-color:#FFE0E0;
-  z-index:0;
-}
-
-.section-1::after {
-  content:"";
-  position:absolute;
-  top:0;
-  left:50%;
-  right:0;
-  bottom:0;
-  background-color:#FFE4E7;
-  z-index:-1;
-}
-
-.section > .img > img {
-  width:100%;
-  display:block;
-}
-
-.section-21-1 {
-  background-color:ffe4e7;
-  z-index:1;
-
-
-
-}
-
-.section-21-1::after {
-  content:"";
-  position:absolute;
-  top:21.87%;
-  height:1035px;
-  left:0;
-  right:0%;
-  bottom:0;
-
-  background-color:#FFE4E7;
-  z-index:-1;
-}
-
-
-.section-21-0 {
-
-  z-index:1;
-  margin-top:100px;
-}
-
-.section-21-0::after {
-  content:"";
-  position:absolute;
-top:29.54%;
-  left:0;
-  right:0;
-  bottom:0;
-  height:476px;
-  background-color:#ffdede;
-  z-index:-1;
-}
-
-
-.section-10 {
-
-  z-index:1;
-  margin-top:100px;
-}
-
-.section-10::after {
-  content:"";
-  position:absolute;
-top:30%;
-  left:0;
-  right:0;
-  bottom:0;
-  height:1131px;
-  background-color:#FFE4E7;
-  z-index:-1;
-  
-}
-
-
-.section-11{
-  z-index:1;
-}
-
-
-
-
-
-.wrap {
-  overflow: hidden;
-}
-
-.con-min-width {
-  min-width: 1280px;
-}
-
-.con {
-  width: 1280px;
-}
-
-.section-8 .part-1 {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
-  overflow: hidden;
-  transform: scale(0.6);
-}
-
-.section {
-  position: relative;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-}
-
-.section > .img > img {
-  width:100%;
-  display:block;
-}
-
-
-
-
-
-.section-23{
-  height:644px;
-}
-.section-22-1 {
-  height: 539px;
-}
-
-.section-24{
-  height:713px;
-}
-
-.section-25-1 {
-  margin-top:60px;
-
-}
-
-
-
-.section-23 {
-  height: 644px;
-}
-
-
-
-.section-2 {
-  margin-top: 80px;
-}
-
-.section-3 {
-  margin-top: 100px;
-}
-
-.section-5 {
-  margin-top: 100px;
-}
-
-.section-6 {
-  margin-top: 100px;
-}
-
-.section-7 {
-  margin-top: 80px;
-}
-
-.section-14{
-  height:822px;
- margin-top:100px;
-}
-
-.section-15{
-  height:1187px;
-}
-
-.section-8 {
-  margin-top: 60px;
-}
-.section-9 {
-  margin-top: 80px;
-}
-
-.section-25 {
-  height: 1282px;
-}
-
-
-
-.section-17{
-  margin-top:100px;
-  height:729px;
-}
-
-.section-18{
-  height:1143px;
-}
-
-.section-19{
-  height:556px;
-}
-
-::-webkit-scrollbar-track
-{
-  background-color: white;
-}
-.section-22{
-  height:1304px;
-}
-
-.section-14{
-  height:822px;
-}
-
-
-.section-15{
-  height:1187px;
-}
-
-
-::-webkit-scrollbar
-{
-  width: 12px;
-  height: 5px;
-  background-color: white;
-}
-
-::-webkit-scrollbar-thumb
-{
-  background-image: linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%);
-}
-
+gsap.from('.section-2', {
+  y:-100,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-2',
+    markers:true,
+    start:'40% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-4', {
+  x:-600,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-4',
+    markers:true,
+    start:'50% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-5', {
+  x:400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-5',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.section-6', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-6',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+gsap.from('.section-7', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-7',
+    markers:true,
+    start:'30% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+
+
+
+gsap.from('.section-9', {
+  x:-400,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.section-9',
+    markers:true,
+    start:'30% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.last-page', {
+  x:-300,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.last-page',
+    markers:true,
+    start:'10% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+gsap.from('.category', {
+  x:-600,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:'.category',
+    markers:true,
+    start:'20% bottom',
+    toggleActions: 'restart none none none'
+  }
+});
+
+
+const Timeline__Section2__1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.section-8',
+    markers:true,
+    start:'top 70%',
+    end:'bottom top',
+    toggleActions: "restart none none none"
+  }
+});
+
+Timeline__Section2__1.from('.section-2__txt-1', {
+  opacity:0,
+  y:-80,
+  duration:1
+});
+
+Timeline__Section2__1.from('.section-2__txt-2', {
+  opacity:0,
+  y:80,
+  duration:1
+}, "-=0.5");
